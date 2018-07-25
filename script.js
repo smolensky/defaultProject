b7dd715da36d8b1e82c37ac9cab5b92e26733328
bsmolenskyi <bohdan.smolensky@gmail.com>
2018-07-25 13:37:58 +0300

    webstorm files, changes to js

--- script.js
+++ script.js
@@ -1,70 +1,22 @@
-//document.writeln('hello');
-var item = {
-	id: 1,
-	title: 'none',
-	description: 'none',
-	status: 'none'
-};
-var itemList = [];
+var list = document.querySelector('ul');
+var item = { id: -1, task: "none", status: "none"};
 
-function createItem(a){
-	debugger;
-	item.title = a.itemTitle.value;
-	item.description = a.itemDescription.value;
-	
-	return item;
+function addItem() {
+    var ul = document.getElementById("itemsList");
+    var li = document.createElement("li");
+    var a = document.getElementById("itemText");
+    var itemText = a.value;
+    var t = document.createTextNode(itemText);
+
+    li.appendChild(t);
+    ul.appendChild(li);
+
+    //clear text input
+    document.getElementById("itemText").value = "";
 }
 
-function setItemStatus(a){
-	if(a !== ''){ 
-		return item.status = a; 
-	} else {
-		return item.status = 'planned';
-	}
-}
-
-function saveItem(){
-	debugger;
-	var draftItem = createItem(this);
-	draftItem.status = setItemStatus('');
-	
-	itemList.push(draftItem);
-}
-
-
-
-
-function addNewItem(){
-	var title = this.itemTitle.value;
-	var description = this.itemDescription.value;
-	
-	item.title = title;
-	item.description = description;
-	
-	itemList.unshift(item);
-	
-	updateItemList(itemList);
-}
-
-function updateItemList(itemList) {
-	var newItemContainer = document.getElementById('item');
-	var itemLi = document.createElement('li');
-	var itemTitle1 = document.createTextNode(item.title);
-	var itemDescription1 = document.createTextNode(item.description);
-	
-	
-	itemLi.appendChild(itemTitle1);
-	itemLi.appendChild(itemDescription1);
-	
-	newItemContainer.appendChild(itemLi);
-	debugger;
-}
-
-
-
-
-
-function showArrayAsItemList(o){
-	
-}
-
+list.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+    }
+}, false);
